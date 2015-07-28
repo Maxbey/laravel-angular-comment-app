5463cfc038e718cc00cfad3699549685c3d490d6
Maxbey <maximbeiner@gmail.com>
2015-07-28 15:17:35 +0600

    CommentsController, CommentRequest, configure routes

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CommentRequest;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Comment;
+
+class CommentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $comments = Comment::all();
+
+        return $comments;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request $request
+     * @return Response
+     */
+    public function store(CommentRequest $request)
+    {
+        Comment::create($request->all());
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $comment = Comment::find($id);
+
+        return $comment;
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request $request
+     * @param  int $id
+     * @return Response
+     */
+    public function update(CommentRequest $request, $id)
+    {
+        $comment = Comment::find($id);
+        $comment->update($request->all());
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        Comment::destroy($id);
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,2 +1,12 @@
 <?php
 
+Route::get('/', function(){
+    return view('index');
+});
+
+Route::group(['prefix' => 'api'], function() {
+
+    Route::resource('comments', 'CommentsController',
+
+        ['only' => ['index', 'store', 'destroy']]);
+});
